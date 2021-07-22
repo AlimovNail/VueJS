2eac4f7c3ab88ab9e0fb5a6b68af166273627b1c
Nail <nail_al@mail.ru>
2021-07-22 18:20:09 +0300

    Rename

---
+++ coverage/lcov-report/Homework-7/src/components/PaymentsList.vue
@@ -0,0 +1,113 @@
+<template>
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Date</th>
+                    <th>Category</th>
+                    <th>Amount</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(payment, index) in paymentsList" :key="payment.number">
+                    <td>{{ payment.number }}</td>
+                    <td>{{ payment.date }}</td>
+                    <td>{{ payment.category }}</td>
+                    <td>{{ payment.amount }}</td>
+                    <button class="icon-btn" @click="showContextMenu(payment)">
+                        <i class="fas fa-ellipsis-v"></i>
+                    </button>
+                    <ContextMenu v-if="showMenuForID === payment.id" :actions="contextActions" :item="{ ...payments[index] }" />
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+import ContextMenu from "./ContextMenu.vue";
+import formatDate from "../utils/formatDate.js";
+export default {
+    name: "PaymentsList",
+    components: {
+        ContextMenu,
+    },
+    props: {
+        payments: {
+            type: Array,
+            default: () => [],
+        },
+    },
+
+    data() {
+        return {
+            paymentActions: [
+                { name: "Edit", comp: "EditPaymentForm", itemName: "Payment" },
+                { name: "Delete", comp: "ConfirmWindow", itemName: "Payment" },
+            ],
+            showMenuForID: null,
+            contextActions: null,
+        };
+    },
+
+    computed: {
+        paymentsList() {
+            const formatedPayments = this.payments.map((payment) => ({ ...payment }));
+            formatedPayments.forEach((payment) => {
+                payment.date = formatDate(new Date(payment.date));
+            });
+
+            return formatedPayments;
+        },
+    },
+
+    methods: {
+        showContextMenu(payment) {
+            if (!this.showMenuForID) {
+                this.$context.show(payment, this.paymentActions);
+            } else {
+                this.$context.hide();
+            }
+        },
+    },
+
+    mounted() {
+        this.$context.EventBus.$on("show", (data) => {
+            this.contextActions = data.actions;
+            this.showMenuForID = data.item.id;
+        });
+
+        this.$context.EventBus.$on("hide", () => {
+            this.showMenuForID = null;
+        });
+    },
+};
+</script>
+<style scoped>
+table {
+    border-collapse: collapse;
+    text-align: left;
+}
+
+tr {
+    border-bottom: 1px solid #333
+}
+
+td,
+th {
+    padding: 15px 15px 5px 5px
+}
+
+button {
+    background: #25a79a;
+    border: none;
+    cursor: pointer;
+    color: #fff;
+}
+
+button:hover {
+    background: #fff;
+    color: #25a79a;
+    border: 1px solid #25a79a;
+}
+</style>
---
+++ coverage/lcov-report/Homework-7/src/router/index.js
@@ -0,0 +1,44 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: { name: 'Dashboard' },
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: () => import(/* webpackChunkName: "PageDashboard" */ '../pages/PageDashboard.vue'),
+    },
+    {
+      path: '/dashboard/:page',
+      component: () => import(/* webpackChunkName: "PageDashboard" */ '../pages/PageDashboard.vue'),
+    },
+    {
+      path: '/about*',
+      name: 'About',
+      component: () => import(/* webpackChunkName: "PageAbout" */ '../pages/PageAbout.vue'),
+    },
+    {
+      path: '/add/payment/:category',
+      component: () => import(/* webpackChunkName: "PageDashboard" */ '../pages/PageDashboard.vue'),
+      props: { openPaymentForm: true },
+    },
+    {
+      path: '/404',
+      name: 'NotFound',
+      component: () => import(/* webpackChunkName: "Page404" */'../pages/Page404.vue'),
+    },
+    {
+      path: '*',
+      redirect: { name: 'NotFound' }
+    },
+  ]
+})
