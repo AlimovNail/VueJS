187e070324e8b0c77ce3c0d1e9109cb3d78b099a
Nail <nail_al@mail.ru>
2021-07-22 17:59:43 +0300

    Homework-7

---
+++ src/components/ConfirmWindow.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        {{ settings.name }} {{ settings.itemName }}? <br />
+        <button @click="confirmAction">Yes</button>
+        <button @click="closeWindow">Cancel</button>
+    </div>
+</template>
+<script>
+export default {
+    name: "ConfirmWindow",
+    props: {
+        item: Object,
+        settings: Object,
+    },
+
+    methods: {
+        confirmAction() {
+            this.$context.emitAction(
+                this.item,
+                this.settings.name + this.settings.itemName
+            );
+            this.closeWindow();
+            this.$context.hide();
+        },
+        closeWindow() {
+            this.$context.hideContentWindow();
+        },
+    },
+};
+</script>
+<style scoped>
+button {
+    margin: 10px;
+    cursor: pointer;
+    border: 1px solid #25a79a;
+    background-color: transparent;
+    width: 70px;
+}
+
+button:hover {
+    background-color: #25a79a;
+    color: #fff;
+}
+</style>
